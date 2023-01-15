b6f4727910d561111c6ff9b4f5da870154261426
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-01-15 13:26:00 +0100

    Added site info

---
+++ src/scripts-ssr/constants.ts
@@ -0,0 +1,4 @@
+export const AUTHOR = "YMSTNT";
+export const AUTHOR_URL = "https://github.com/YMSTNT";
+export const ICON = "https://ymstnt.com/tnt512.png?size=256";
+export const THEME_COLOR = "#00338f";
