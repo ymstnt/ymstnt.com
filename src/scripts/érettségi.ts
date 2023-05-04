38e28dd85c18fa856fd3cb641806cf76dceeafc8
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-05-04 13:47:08 +0200

    Érettségi: all years are now working, IT too

--- src/scripts/érettségi.ts
+++ src/scripts/érettségi.ts
@@ -12,188 +12,203 @@ class DaySubject {
 
 function updateDays() {
   const currentDate: Date = new Date();
-  const targetDate: Date = new Date('2023-05-08');
-  const finalDate: Date = new Date('2023-07-01');
+  const targetDate: Date = new Date("2023-05-08");
+  const finalDate: Date = new Date("2023-07-01");
 
   const timeDiff: number = targetDate.getTime() - currentDate.getTime();
   const daysLeft: number = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-  var exams: DaySubject[] = [
-    new DaySubject(new Date('2023-05-08 09:00'), 'Magyar nyelv és irodalom', "középszintű"),
-    new DaySubject(new Date('2023-05-09 09:00'), 'Matematika', "középszintű"),
-    new DaySubject(new Date('2023-05-10 09:00'), 'Történelem', "középszintű"),
-    new DaySubject(new Date('2023-05-11 09:00'), 'Angol nyelv', "középszintű"),
-    new DaySubject(new Date('2023-05-11 08:00'), 'Informatika', "középszintű"),
-    new DaySubject(new Date('2023-05-17 09:00'), 'Informatika ismeretek', "középszintű és emelt"),
-    new DaySubject(new Date('2023-05-22 08:00'), 'Informatika', "emelt"),
+  let exams: DaySubject[] = [
+    new DaySubject(
+      new Date("2023-05-08 09:00"),
+      "Magyar nyelv és irodalom",
+      "középszintű"
+    ),
+    new DaySubject(new Date("2023-05-09 09:00"), "Matematika", "középszintű"),
+    new DaySubject(new Date("2023-05-10 09:00"), "Történelem", "középszintű"),
+    new DaySubject(new Date("2023-05-11 09:00"), "Angol nyelv", "középszintű"),
+    new DaySubject(new Date("2023-05-11 08:00"), "Informatika", "középszintű"),
+    new DaySubject(
+      new Date("2023-05-17 09:00"),
+      "Informatika ismeretek",
+      "középszintű és emelt"
+    ),
+    new DaySubject(new Date("2023-05-22 08:00"), "Informatika", "emelt"),
   ];
 
   if (currentDate < targetDate) {
-    document.querySelector('#countdown').textContent = daysLeft.toString() + " nap";
+    document.querySelector("#countdown").textContent =
+      daysLeft.toString() + " nap";
   } else if (currentDate >= finalDate) {
-    document.querySelector('#countdown-intro').textContent = "Vége az érettségi időszaknak.";
+    document.querySelector("#countdown-intro").textContent =
+      "Vége az érettségi időszaknak.";
   } else {
-    exams.forEach(element => {
+    exams.forEach((element) => {
       if (element.dateTime.getDate() == currentDate.getDate()) {
-        document.querySelector('#countdown-intro').textContent = "A mai érettségi vizsga:";
-        document.querySelector('#countdown').textContent = element.subject + " (" + element.type + ")";
-        document.querySelector('#when').textContent = "Kezdés: " + element.dateTime.toLocaleTimeString('hu-HU', { hour: '2-digit', minute: '2-digit' }) + "-kor";
+        document.querySelector("#countdown-intro").textContent =
+          "A mai érettségi vizsga:";
+        document.querySelector("#countdown").textContent =
+          element.subject + " (" + element.type + ")";
+        document.querySelector("#when").textContent =
+          "Kezdés: " +
+          element.dateTime.toLocaleTimeString("hu-HU", {
+            hour: "2-digit",
+            minute: "2-digit",
+          }) +
+          "-kor";
       } else {
-        document.querySelector('#countdown-intro').textContent = "Pihenőnap 🌙";
+        document.querySelector("#countdown-intro").textContent = "Pihenőnap 🌙";
       }
     });
   }
-
-  
 }
+
+let generatedLink: string;
+let pressedButton: string;
+
 updateDays();
 formUpdate();
 
-var generatedLink: string;
-var pressedButton: string;
+
 
 function formUpdate() {
   console.log("Form updated");
-  var year = (<HTMLInputElement>document.querySelector('#year')).value;
-  var period = (<HTMLInputElement>document.querySelector('#period')).value;
-  var difficulty = (<HTMLInputElement>document.querySelector('#difficulty')).value;
-  var subject = (<HTMLInputElement>document.querySelector('#subject')).value;
+  let year = (<HTMLInputElement>document.querySelector("#year")).value;
+  let period = (<HTMLInputElement>document.querySelector("#period")).value;
+  let difficulty = (<HTMLInputElement>document.querySelector("#difficulty"))
+    .value;
+  let subject = (<HTMLInputElement>document.querySelector("#subject")).value;
 
-  var finalPeriod: string;
-  var periodMonth: string;
-  var finalDifficulty: string;
-  var finalSubject: string;
-  var finalType: string;
-  var fileType: string;
+  let generatedPeriod: string;
+  let periodMonth: string;
+  let generatedDifficulty: string;
+  let generatedSubjects: string;
+  let generatedFileType: string;
+  let itFileType: string;
 
   if (period == "may") {
-    finalPeriod = "tavasz";
+    generatedPeriod = "tavasz";
     periodMonth = "maj";
-  }
-  else
-  {
-    finalPeriod = "osz";
+  } else {
+    generatedPeriod = "osz";
     periodMonth = "okt";
   }
 
   if (difficulty == "medium") {
-    finalDifficulty = "kozep";
-  }
-  else
-  {
-    finalDifficulty = "emelt";
+    generatedDifficulty = "kozep";
+  } else {
+    generatedDifficulty = "emelt";
   }
 
   if (subject == "it-office" || subject == "it") {
-    (<HTMLInputElement>document.querySelector('#sourcefiles')).disabled = false;
-    (<HTMLInputElement>document.querySelector('#solutionfiles')).disabled = false;
+    (<HTMLInputElement>document.querySelector("#sourcefiles")).disabled = false;
+    (<HTMLInputElement>document.querySelector("#solutionfiles")).disabled =
+      false;
+  } else {
+    (<HTMLInputElement>document.querySelector("#sourcefiles")).disabled = true;
+    (<HTMLInputElement>document.querySelector("#solutionfiles")).disabled =
+      true;
   }
-  else
-  {
-    (<HTMLInputElement>document.querySelector('#sourcefiles')).disabled = true;
-    (<HTMLInputElement>document.querySelector('#solutionfiles')).disabled = true;
-  }
+
+  let convertedYear: number = parseInt(year);
 
   switch (subject) {
     case "hungarian":
-      finalSubject = "magyir";
+      generatedSubjects = "magyir";
       break;
     case "mathematics":
-      finalSubject = "mat";
+      generatedSubjects = "mat";
       break;
     case "history":
-      finalSubject = "tort";
+      generatedSubjects = "tort";
       break;
     case "english":
-      finalSubject = "angol";
+      generatedSubjects = "angol";
       break;
     case "it":
-      finalSubject = "infoism";
+      generatedSubjects = "infoism";
       break;
     case "it-office":
-      finalSubject = "inf";
+      if (convertedYear <= 2011 && !(convertedYear == 2011 && period == "october")) {
+        generatedSubjects = "info";
+      } else {
+        generatedSubjects = "inf";
+      }
       break;
     default:
       break;
   }
+
+  let linkPrefix: string = "https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok";
 
   switch (pressedButton) {
     case "task":
-      finalType = "fl.pdf";
-      fileType = "";
+      generatedFileType = "fl.pdf";
+      itFileType = "";
       break;
     case "sourcefiles":
-      finalType = "fl.zip";
-      fileType = "for";
+      if (convertedYear >= 2005 && convertedYear <= 2008) {
+        itFileType = "forras";
+      } else {
+        itFileType = "for";
+      }
+      generatedFileType = "fl.zip";
       break;
     case "solution":
-      finalType = "ut.pdf";
-      fileType = "";
+      generatedFileType = "ut.pdf";
+      itFileType = "";
       break;
     case "solutionfiles":
-      finalType = "ut.zip";
-      fileType = "meg";
+      if (convertedYear >= 2005 && convertedYear <= 2008) {
+        itFileType = "megoldas";
+      } else {
+        itFileType = "meg";
+      }
+      generatedFileType = "ut.zip";
       break;
     default:
-      finalType = "fl.pdf";
-      fileType = "";
+      generatedFileType = "fl.pdf";
+      itFileType = "";
       break;
   }
 
-  var convertedYear: number = +year;
+  if (convertedYear > 2012) {
+    linkPrefix = linkPrefix + '_';
+  }
+
+  let firstCharDifficulty: string = generatedDifficulty.charAt(0);
+  let cutYear: string = year.slice(-2);
+
+  let assembledPart: string;
 
   if (convertedYear == 2005 && period == "may") {
-    if (pressedButton == "sourcefiles") {
-      fileType = "forras";
-    }
-    else if (pressedButton == "solutionfiles") {
-      fileType = "megoldas";
-    }
-    else {
-      fileType = "";
-    }
-
-    generatedLink = `https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok2005tavasz/${finalDifficulty}/${finalDifficulty.charAt(0)}_${finalSubject}${fileType}_${finalType}`
+    assembledPart = `${year}${generatedPeriod}/${generatedDifficulty}/${firstCharDifficulty}_${generatedSubjects}${itFileType}_${generatedFileType}`;
   }
-  else if (convertedYear > 2005 && convertedYear <= 2008) {
-    if (pressedButton == "sourcefiles") {
-      fileType = "forras";
-    }
-    else if (pressedButton == "solutionfiles") {
-      fileType = "megoldas";
-    }
-    else {
-      fileType = "";
-    }
-
-    generatedLink = `https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok${year}${finalPeriod}/${finalDifficulty}/${finalDifficulty.charAt(0)}_${finalSubject}${fileType}_${year.slice(-2)}${periodMonth}_${finalType}`
-  }
-  else if (convertedYear > 2008 && convertedYear <= 2012) {
-    generatedLink = `https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok${year}${finalPeriod}/${finalDifficulty}/${finalDifficulty.charAt(0)}_${finalSubject}${fileType}_${year.slice(-2)}${periodMonth}_${finalType}`
-  }
-  else if (convertedYear == 2012 && period == "october") {
-    generatedLink = `https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok_${year}${finalPeriod}_${finalDifficulty}/${finalDifficulty.charAt(0)}_${finalSubject}${fileType}_${year.slice(-2)}${periodMonth}_${finalType}`;
+  else if (convertedYear >= 2005 && convertedYear <= 2012 && !(convertedYear == 2005 && period == "may") && !(convertedYear == 2012 && period == "october")) {
+    assembledPart = `${year}${generatedPeriod}/${generatedDifficulty}/${firstCharDifficulty}_${generatedSubjects}${itFileType}_${cutYear}${periodMonth}_${generatedFileType}`;
   }
   else {
-    generatedLink = `https://www.oktatas.hu/bin/content/dload/erettsegi/feladatok_${year}${finalPeriod}_${finalDifficulty}/${finalDifficulty.charAt(0)}_${finalSubject}${fileType}_${year.slice(-2)}${periodMonth}_${finalType}`;
+    assembledPart = `${year}${generatedPeriod}_${generatedDifficulty}/${firstCharDifficulty}_${generatedSubjects}${itFileType}_${cutYear}${periodMonth}_${generatedFileType}`;
   }
-  
-  (<HTMLInputElement>document.querySelector('#output')).value = generatedLink;
+
+  generatedLink = linkPrefix + assembledPart;
+
+  (<HTMLInputElement>document.querySelector("#output")).value = generatedLink;
 }
 
-const dropdowns = Array.from(document.getElementsByClassName('dropdown'));
+const dropdowns = Array.from(document.getElementsByClassName("dropdown"));
 
-dropdowns.forEach(dropdown => {
-  dropdown.addEventListener('change', formUpdate);
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("change", formUpdate);
 });
 
-const buttons = Array.from(document.getElementsByClassName('btn'));
+const buttons = Array.from(document.getElementsByClassName("btn"));
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', (e) => {
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     let element = e.target;
     pressedButton = (<HTMLInputElement>element).id;
     formUpdate();
-    window.open(generatedLink, '_blank');
+    window.open(generatedLink, "_blank");
   });
 });
