9f6dec920e24fb6b98dc360264919e214ad2d952
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-02-16 22:26:25 +0100

    egyetem: run script on every page load and check exam periods - Now egyetem script runs on every page load, including view transition loads - egyetem script is overhauled: instead of first week, we can store exam periods and it checks against them - if it's an ongoing exam period, it will show that instead - if it's not an ongoing exam period, it will calculate using the last exam period's end date and show the correct week number - egyetem page: adjust description and show a default "." to avoid ugly css resizing

--- src/scripts/egyetem.ts
+++ src/scripts/egyetem.ts
@@ -1,23 +1,44 @@
-let firstWeek: Date = new Date(2024, 1, 12);
+let examPeriods: { start: Date; end: Date }[] = [
+  //make sure to format dates correctly
+  //months are indexed from 0 to 11, 0 being January
+  //exam dates should end on a Monday (beginning of new semester)
+  { start: new Date(2023, 11, 18), end: new Date(2024, 1, 12) },
+  { start: new Date(2024, 4, 20), end: new Date(2024, 5, 29) },
+];
 
-function checkDateBefore(givenDate: Date): void {
-  let currentDate = new Date();
+let currentDate = new Date();
 
-  if (currentDate < givenDate){
+function checkDateBefore(): void {
+  console.log(currentDate.getDay());
+  let isInExamPeriod = examPeriods.some(period => currentDate >= period.start && currentDate <= period.end);
+  if (isInExamPeriod){
     (<HTMLInputElement>document.querySelector('#week-number')).innerText = "Vizsgaidőszak - szünet";
     (<HTMLInputElement>document.querySelector('#week-number')).style.fontSize = "1.2em";
   } else {
-    calculateWeekNumber(firstWeek);
+    let lastExamPeriodEnd = getLastExamPeriodEnd();
+    calculateWeekNumber(lastExamPeriodEnd);
   };
 }
 
-function calculateWeekNumber(givenDate: Date): void {
-  let oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000;
-  let currentDate = new Date();
-  
-  let timeDifference = currentDate.getTime() - givenDate.getTime();
-  let weeksPassed = Math.ceil(timeDifference / oneWeekInMilliseconds);
-  (<HTMLInputElement>document.querySelector('#week-number')).innerText = weeksPassed.toString() + ".";
+function getLastExamPeriodEnd(): Date {
+  let lastExamPeriodEnd = new Date();
+
+  for (const period of examPeriods) {
+    if (period.end < currentDate) {
+      lastExamPeriodEnd = period.end;
+    }
+  }
+
+  return lastExamPeriodEnd;
 }
 
-checkDateBefore(firstWeek);
+function calculateWeekNumber(firstWeek: Date): void {
+  let oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000;
+  
+  let timeDifference = currentDate.getTime() - firstWeek.getTime();
+  let weeksPassed = Math.floor(timeDifference / oneWeekInMilliseconds) + 1;
+
+  (<HTMLInputElement>document.querySelector('#week-number')).innerText = (weeksPassed).toString() + ".";
+}
+
+document.addEventListener("astro:page-load", () => checkDateBefore());
